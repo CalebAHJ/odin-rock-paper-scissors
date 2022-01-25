2053b690c3c245c6930da115a37e6de177b0e7b1
CalebAHJ <calebharperjohnson@gmail.com>
2022-01-25 13:18:01 -0600

    score, round, and game finish displaying correctly

--- rps.js
+++ rps.js
@@ -8,6 +8,32 @@ function computerPlay() {
     }
 }
 
+function win() {
+    const roundCount = document.getElementById('round');
+    let roundNum = +roundCount.textContent.charAt(6);
+    roundNum++;
+    roundCount.textContent = "Round " + roundNum;
+
+    const score = document.getElementById('score');
+    let playerScore = +score.textContent.charAt(6);
+    playerScore++;
+    let comScore = score.textContent.charAt(8);
+    score.textContent = "Score " + playerScore + "-" + comScore;
+}
+
+function lose() {
+    const roundCount = document.getElementById('round');
+    let roundNum = +roundCount.textContent.charAt(6);
+    roundNum++;
+    roundCount.textContent = "Round " + roundNum;
+
+    const score = document.getElementById('score');
+    let playerScore = score.textContent.charAt(6);
+    let comScore = +score.textContent.charAt(8);
+    comScore++;
+    score.textContent = "Score " + playerScore + "-" + comScore;
+}
+
 function round(playerSelection, computerSelection) {
     const msg = document.getElementById("msg");
 
@@ -18,9 +44,11 @@ function round(playerSelection, computerSelection) {
             }
             else if (computerSelection === "paper") {
                  msg.textContent = "You Lose! Paper beats Rock";
+                 lose();
             }
             else {
                 msg.textContent = "You Win! Rock beats Scissors";
+                win();
             }
         }
         break;
@@ -28,12 +56,14 @@ function round(playerSelection, computerSelection) {
         case "paper": {
             if (computerSelection === "rock") {
                 msg.textContent = "You Win! Paper beats Rock";
+                win();
             }
             else if (computerSelection === "paper") {
                 msg.textContent = "Tie! Both Paper";
             }
             else {
                 msg.textContent = "You Lose! Scissors beats Paper";
+                lose();
             }
         }
         break;
@@ -41,9 +71,11 @@ function round(playerSelection, computerSelection) {
         case "scissors": {
             if (computerSelection === "rock") {
                 msg.textContent = "You Lose! Rock beats Scissors";
+                lose();
             }
             else if (computerSelection === "paper") {
                 msg.textContent = "You Win! Scissors beats Paper";
+                win();
             }
             else {
                 msg.textContent = "Tie! Both Scissors";
@@ -53,13 +85,26 @@ function round(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+function isDone() {
+    const score = document.getElementById('score');
+    const pScore = +score.textContent.charAt(6);
+    const cScore = +score.textContent.charAt(8);
 
-    let roundCount = 1;
+    if (pScore < 3 && cScore < 3) return;
+    
+    if (pScore === 3) document.getElementById('header').textContent = "Congrats! You Won!";
+    else document.getElementById('header').textContent = "Aww! You Lost. ";
+
+    window.addEventListener('click', () => window.location.reload());
+}
+
+function game() {
 
     const buttons = document.querySelectorAll('button');
     buttons.forEach((button) => button.addEventListener('click', 
     () => round(button.id, computerPlay())));
+
+    buttons.forEach((button) => button.addEventListener('click', isDone));
 }
 
 game()
